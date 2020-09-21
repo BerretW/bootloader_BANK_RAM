123d7157d5462cadf55e67f67c3a58e4212febfb
Adolf01 <adolf01@seznam.cz>
2020-09-21 18:52:56 +0200

    clean 2

--- makefile.bat
+++ makefile.bat
@@ -1,5 +1,8 @@
+for %%a in (".\src\*.asm") do (
+ca65.exe -I .\include -o .\output\%%~na.o .\%%a
+)
 cd .\output
 cl65.exe main.o acia.o zeropage.o ram_disk.o ewoz.o -C ..\config\APP_RAM_DISK.cfg -o ramdisk.bin
 cl65.exe main.o acia.o zeropage.o ram_disk.o ewoz.o -C ..\config\appartus.cfg -o ROM.bin
 cl65.exe main.o acia.o zeropage.o ram_disk.o ewoz.o -C ..\config\bank_ram_disk.cfg -o BANK.bin
-pause
+

de2a5ecdcadc7e54358f3c3538a01a7157f982ae
adolf01 <adolf01@seznam.cz>
2021-01-10 21:26:27 +0100

    aa

--- src/jumptable.asm
+++ src/jumptable.asm
@@ -12,7 +12,7 @@ _song_pos:			.res 2
 .segment "JMPTBL"
 
 CHROUT: JMP _acia_putc                  ;FF00 print CHAR from regA
-CHRIN: JMP _acia_getc                   ;FF03 get char from buffer to regA
+CHRIN:  JMP _acia_getc                  ;FF03 get char from buffer to regA
 PRNTLN: JMP _print_nl                   ;FF06 put new line and a string with start address in regA and regX "lda #<(STRING),ldx #>(STRING),jsr PRNTLN"
 PRNT:   JMP _acia_puts                  ;FF09 put a string with start address in regA and regX "lda #<(STRING),ldx #>(STRING),jsr PRNTLN"
 SETBNK: JMP _set_bank                   ;FF0C set bank to number from regA
@@ -20,4 +20,4 @@ GETBNK: JMP _get_bank                   ;FF0F get bank number to regA
 SNINIT: JMP _sn_init                    ;FF12 Initialize SN76489 chipwith mute
 SNWRT:  JMP _sn_write_data              ;FF15 write data from regA to sn76489
 SHDLY:  JMP __delay2                    ;FF18 very short delay
-RST:    JMP _loop                        ;FF1B
+RST:    JMP _loop                       ;FF1B
